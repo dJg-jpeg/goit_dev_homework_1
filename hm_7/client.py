364db98dc20b651d09e18b0cbba04c907d546a6b
Volodymyr <vvl5656@gmail.com>
2022-04-27 17:09:31 +0300

    complete chat as post box realization

--- hm_7/client.py
+++ hm_7/client.py
@@ -1,13 +1,32 @@
+"""If you want to start messaging with connection to another PC , please run this file"""
 from socket import socket
+from configparser import ConfigParser
 
 
-def send(host, port, message):
+def send(server_ip, server_port, message):
     with socket() as s:
         try:
-            s.connect((host, port))
+            s.connect((server_ip, server_port))
             s.send(message.encode("UTF-8"))
-            data = s.recv(16)
-            if data.decode("UTF-8") == "OK":
-                return "Your message was successfully received"
+            data = s.recv(256)
+            if data:
+                print(f"|Another PC| : {data.decode('UTF-8')}")
         except (ConnectionRefusedError, ConnectionError, ConnectionResetError, ConnectionAbortedError, TimeoutError):
-            return "Something went wrong , please try again"
+            print(f"//Something went wrong , please try again//")
+
+
+def user_interface(server_ip, server_port):
+    while True:
+        message = input("|Me| : ")
+        if message == "exit":
+            send(server_ip, server_port, message)
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read('ips.ini')
+    this_pc = config.get("IPS", "this_pc_ip")
+    another_pc = config.get("IPS", "another_computer_ip")
+    port = int(config.get("IPS", "port"))
+    print("Print exit to exit the app...\n\n\n")
+    user_interface(another_pc, port)
--- hm_7/server.py
+++ hm_7/server.py
@@ -1,14 +1,41 @@
+"""If you want to start messaging with a server on your pc , please run this file"""
 from socket import socket, SOL_SOCKET, SO_REUSEADDR
+from threading import Thread
+from configparser import ConfigParser
+from client import user_interface
 
 
-def receive(host, port):
+def message_server(host, server_port, clients_ips):
+    answers = {clients_ips[0]: '',
+               clients_ips[1]: '',
+               }
     with socket() as s:
         s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        s.bind((host, port))
-        s.listen(1)
-        connection, address = s.accept()
-        print(f"Connected from {address}")
-        data = connection.recv(256)
-        connection.send(b"OK")
-        connection.close()
-    return f"Received a new incoming message : {data.decode('UTF-8')}"
+        s.bind((host, server_port))
+        s.listen(2)
+        while True:
+            conn, addr = s.accept()
+            ip = addr[0]
+            data = conn.recv(256)
+            answers[ip] = data
+            for this_ip, this_answer in answers.items():
+                if this_ip == ip:
+                    continue
+                elif len(this_answer) == 0:
+                    break
+                conn.send(this_answer)
+                answers[this_ip] = ''
+            conn.close()
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read('ips.ini')
+    this_pc = config.get("IPS", "this_pc_ip")
+    another_pc = config.get("IPS", "another_computer_ip")
+    port = int(config.get("IPS", "port"))
+    print("Print exit to exit the app...\n\n\n")
+    server = Thread(target=message_server, args=(this_pc, port, (this_pc, another_pc)))
+    server.start()
+    user_me = Thread(target=user_interface, args=(this_pc, port))
+    user_me.start()
